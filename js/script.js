529d768f9a666628dc1142b1edfe79676feef935
devGDL <20027735@es.uveg.edu.mx>
2020-12-30 08:07:40 -0600

    Bounce ball

--- js/script.js
+++ js/script.js
@@ -3,19 +3,9 @@ let ctx = myGameCanvas.getContext('2d')
 
 let x = myGameCanvas.width / 2
 let y = myGameCanvas.height - 30
-
 let dx = 2
 let dy = -2
-
-let draw = () => {
-    ctx.beginPath()
-    ctx.arc(x, y, 10, 0, Math.PI * 2)
-    ctx.fillStyle = 'Blue'
-    ctx.fill()
-    ctx.closePath()
-    x += dx
-    y += dy
-}
+let ballRadius = 10
 
 let drawTitle = () => {
     ctx.beginPath()
@@ -25,5 +15,32 @@ let drawTitle = () => {
     ctx.closePath()
 }
 
+let draw = () => {
+    clearTrial()
+    drawBall()
+
+    //Changes direction of ball
+    if (y + dy > myGameCanvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy
+    }
+
+    if (x + dx > myGameCanvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dy
+    }
+
+    x += dx
+    y += dy
+}
+
+let drawBall = () => {
+    ctx.beginPath()
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2)
+    ctx.fillStyle = 'Blue'
+    ctx.fill()
+    ctx.closePath()
+}
+
+let clearTrial = () => ctx.clearRect(0, 0, myGameCanvas.width, myGameCanvas.height)
+
 drawTitle()
-setInterval(draw, 10)
+setInterval(draw, 20)
